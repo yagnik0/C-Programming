f18fbdce7ffbfc42c68fb055f6cb278841663b80
unknown <yashyagnik456@gmail.com>
2025-10-27 06:12:22 +0530

    comments

---
+++ Comments in C/comments.c
@@ -0,0 +1,13 @@
+// There are two types of comments in C programming
+
+// 1) Multi Line Comment
+// 2) Single Line Comment
+
+/*This is a Multi line commnet*/
+
+#include<stdio.h>
+int main(){
+  // This is a single line cooment
+  printf("Hello World");
+  return 0;
+}
