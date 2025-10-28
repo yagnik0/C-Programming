a8a2f35a59873c7b62aa0eec10155afd9a18e4e1
unknown <yashyagnik456@gmail.com>
2025-10-29 04:16:36 +0530

    comments

---
+++ Variable in C/variaable.c
@@ -0,0 +1,13 @@
+// C is a statically type language so we need declare a variable with its type
+
+#include<stdio.h>
+int main() {
+  int x = 10;
+  printf("%d\n", x);
+  int y = 15;
+  printf("%d\n", y);
+  y= 20;
+  printf("%d", y);
+
+  return 0;
+}
